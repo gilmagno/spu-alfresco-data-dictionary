a45131cf2e8111fd1aa372896a63631e72ab185d
Gil Magno <gilmagno@gmail.com>
2011-12-06 15:51:00 -0300

    Tratamento de CPF/CNPJ de manifestante de processo da listagem de processos na Incorporação
    Adicona funcionalidade que converte _'s para /'s em um CPF/CNPJ de manifestante, para que possa ser possível procurar por CPF/CNPJ que contenham /'s. Normalmente /'s quebrariam a consulta, por fazerem parte do URL. Agora é possível procurar por número que tenha /. Na requisição, mudar / para _, e Web Script converterá de volta.

--- Web Scripts Extensions/org/spu/processo/incorporacaocaixaanalise/incorporacaocaixaanalise.get.js
+++ Web Scripts Extensions/org/spu/processo/incorporacaocaixaanalise/incorporacaocaixaanalise.get.js
@@ -3,6 +3,7 @@
 var processoId = (url.templateArgs['processoId']) ? url.templateArgs['processoId'] : null;
 var assuntoId = (url.templateArgs['assuntoId']) ? url.templateArgs['assuntoId'] : null;
 var manifestanteCpf = (url.templateArgs['manifestanteCpf']) ? url.templateArgs['manifestanteCpf'] : null;
+manifestanteCpf = manifestanteCpf.replace('_', '/');
 var offset = (url.templateArgs['offset']) ? url.templateArgs['offset'] : null;
 var pageSize = (url.templateArgs['pageSize']) ? url.templateArgs['pageSize'] : null;
 var filter = url.templateArgs['filter'];
